6412871eedb0700f27e89afa793e5601f6c7d1f2
agung037 <agungk878@gmail.com>
2024-03-10 17:46:45 +0700

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Hai I'm Agung</h1>
+
+
+<h2>Projects : </h2>
+<ul>
+    <li>
+        <a href="/projects/ui/qr">QR Code Component</a>
+    </li>
+</ul>
---
+++ src/routes/projects/ui/qr/+page.svelte
@@ -0,0 +1,26 @@
+
+<script>
+    import '../../../../app.css'
+    import image from '$lib/images/image-qr-code.png';
+</script>
+
+<div class="main h-screen bg-slate-200 flex items-center justify-center">
+
+    <!-- my card -->
+    <div class="p-3 m-5 max-w-md bg-white rounded-lg overflow-hidden shadow-md w-full md:w-2/3 lg:w-1/2 xl:w-1/3">
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <img class="w-full h-50 object-cover rounded-lg" src="{image}" alt="Card Image">
+        <div class="p-4">
+            <div>
+                <h2 class="text-3xl font-semibold mb-2 text-center">improve your front-end skill by building projects</h2>
+            </div>
+            <div>
+                <p class="text-xl text-gray-400 text-center">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+            </div>
+        </div>
+    </div>
+    <!-- end of my card -->
+</div>
+
+<style>
+</style>
